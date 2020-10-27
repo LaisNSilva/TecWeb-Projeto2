de8b0d646711e54870d00bdfd462a0362a5039b6
LaisNSilva <silva.lais72@hotmial.com>
2020-10-27 19:04:39 -0300

    função dando erro

--- projetoNEM/routes/index.js
+++ projetoNEM/routes/index.js
@@ -8,29 +8,6 @@ router.get('/', function (req, res, next) {
 
 module.exports = router;
 
-/* GET Userlist page. */
-router.get('/userlist', function (req, res) {
-  var db = require("../db");
-  var Users = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
-  Users.find({}).lean().exec(
-    function (e, docs) {
-      res.json(docs);
-      res.end();
-    });
-});
-
-/* GET ONE users. */
-router.get('/user/:id', function (req, res, next) {
-  var db = require('../db');
-  var User = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
-  User.find({ _id: req.params.id }).lean().exec(function (e,
-    docs) {
-    res.json(docs);
-    res.end();
-  });
-});
 
 /* GET New User page. */
 router.get('/newuser', function (req, res) {
@@ -62,81 +39,29 @@ router.post('/adduser', function (req, res) {
   });
 });
 
-/* POST ONE users. */
-router.post('/users/', function (req, res, next) {
-  var db = require('../db');
-  var User = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
-  var newuser = new User({
-    username: req.body.name, email:
-      req.body.email
-  });
-  newuser.save(function (err) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      res.end();
-      return;
-    }
-    res.json(newuser);
-    res.end();
-  });
-});
 
-/* PUT ONE user. */
-router.put('/users/:id', function (req, res, next) {
-  var db = require('../db');
-  var User = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
-  User.findOneAndUpdate({ _id: req.params.id }, req.body,
-    { upsert: true }, function (err, doc) {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        res.end();
-        return;
-      }
-      res.json(req.body);
-      res.end();
-    });
-});
-
-/* DELETE ONE user. */
-router.delete('/users/:id', function (req, res, next) {
-  var db = require('../db');
-  var User = db.Mongoose.model('usercollection',
-    db.UserSchema, 'usercollection');
-  User.find({ _id: req.params.id }).remove(function (err) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      res.end();
-      return;
-    }
-    res.json({ success: true });
-    res.end();
-  });
-});
 
 /* Login. */
-router.get('/login', function (req, res, next) {
+router.post('/login', function (req, res, next) {
   var db = require('../db');
-  var User = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
+  var User = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
   var user = User({
-    username: req.body.username, senha:
-      req.body.senha
+    username: req.body.username, senha: req.body.senha
   });
   User.find({user}, (err, users) => {
     // users is an array which may be empty for no resultsif (err) {​​​​
     // handle errorreturn;
     if (users.length) {
       // there are user(s)
-      res.json({success:true})
-      res.end()
+      res.json({success: true});
+      res.end();
 
     } else {
       // there are no users
-      res.json({success:false})
-      res.end()
+      res.json({success: false});
+      res.end();
     }
+  
   }​​​​);
     
 });
