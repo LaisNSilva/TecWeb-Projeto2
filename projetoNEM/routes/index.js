f1b60358e812638318ce80a50209d21045b61edd
fernandocfbf <fernandocfbf@al.insper.edu.br>
2020-10-27 23:01:10 -0300

    Chega os dados de username e senha

--- projetoNEM/routes/index.js
+++ projetoNEM/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
@@ -11,57 +11,57 @@ module.exports = router;
 
 /* GET New User page. */
 router.get('/newuser', function (req, res) {
-  res.render('newuser', { title: 'Add New User' });
+    res.render('newuser', { title: 'Add New User' });
 });
 
 /* POST to Add User Service */
 router.post('/adduser', function (req, res) {
-  var db = require("../db");
-  var Name = req.body.name;
-  var userName = req.body.username;
-  var Email = req.body.email;
-  var senha = req.body.senha;
-  var Users = db.Mongoose.model('usercollection', db.UserSchema,
-    'usercollection');
-  var user = new Users({
-    name: Name, username: userName, email:
-      Email, senha: Senha
-  });
-  user.save(function (err) {
-    if (err) {
-      console.log("Error! " + err.message);
-      return err;
-    }
-    else {
-      console.log("Post saved");
-      res.redirect("userlist");
-    }
-  });
+    var db = require("../db");
+    var Name = req.body.name;
+    var userName = req.body.username;
+    var Email = req.body.email;
+    var senha = req.body.senha;
+    var Users = db.Mongoose.model('usercollection', db.UserSchema,
+        'usercollection');
+    var user = new Users({
+        name: Name, username: userName, email:
+            Email, senha: Senha
+    });
+    user.save(function (err) {
+        if (err) {
+            console.log("Error! " + err.message);
+            return err;
+        }
+        else {
+            console.log("Post saved");
+            res.redirect("userlist");
+        }
+    });
 });
 
+router.post('/login', function (req, res) {
+    var db = require("../db");
+    var Users = db.Mongoose.model('usercollection', db.UserSchema,
+        'usercollection');
 
+    var userName = req.body.username;
+    var userSenha = req.body.senha;
 
-/* Login. */
-router.post('/login', function (req, res, next) {
-  var db = require('../db');
-  var User = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
-  var user = User({
-    username: req.body.username, senha: req.body.senha
-  });
-  User.find({user}, (err, users) => {
-    // users is an array which may be empty for no resultsif (err) {​​​​
-    // handle errorreturn;
-    if (users.length) {
-      // there are user(s)
-      res.json({success: true});
-      res.end();
+    var user = new Users({
+        username: userName,
+        senha: userSenha
+    });
 
-    } else {
-      // there are no users
-      res.json({success: false});
-      res.end();
-    }
-  
-  }​​​​);
-    
+    Users.find({ user }, (err, users) => {
+        if (users.length) {
+            // there are user(s)
+            res.json({ success: true });
+            res.end();
+
+        } else {
+            // there are no users
+            res.json({ success: false });
+            res.end();
+        }
+    });
 });
